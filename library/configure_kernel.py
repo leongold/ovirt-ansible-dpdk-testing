c07fb7a06b4d7eda0a7b977bd23944f5862dae25
Leon Goldberg <lgoldber@redhat.com>
2018-07-14 22:08:37 +0300

    werk

--- library/configure_kernel.py
+++ library/configure_kernel.py
@@ -145,7 +145,7 @@ def _get_kernel_args():
 
 def _select_cpu_partitioning(cpu_list):
     profile = 'cpu-partitioning' if cpu_list else 'default'
-    rc, _, err = _exec_cmd(['tuned-adm', 'profile', default])
+    rc, _, err = _exec_cmd(['tuned-adm', 'profile', profile])
     if rc != 0:
         raise SelectCpuPartitioningError(err)
 
